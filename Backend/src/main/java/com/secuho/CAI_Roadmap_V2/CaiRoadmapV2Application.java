02b1c305613ff2b7fd3f494f342e9c13daafb27b
Secuho <secuho@secuho.life>
2026-07-15 22:00:31 +0900

    [Init] Spring Boot Backend Initiallizing.

---
+++ Backend/src/main/java/com/secuho/CAI_Roadmap_V2/CaiRoadmapV2Application.java
@@ -0,0 +1,13 @@
+package com.secuho.CAI_Roadmap_V2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaiRoadmapV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaiRoadmapV2Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/secuho/CAI_Roadmap_V2/CaiRoadmapV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secuho.CAI_Roadmap_V2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaiRoadmapV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
